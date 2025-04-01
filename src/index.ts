44b26b617b4c4ee22d4d2c127ae73e94287a768f
Ruan Chagas <ruanchagas95@gmail.com>
2025-04-01 15:29:15 -0300

    feat: implement main function to scrape product details from multiple pages just logging the contents

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
+import { scraperDetails } from "./webscraper/scraperDetails";
+import { scraperLinks } from "./webscraper/scraperLinks";
+import { scraperMaxPage } from "./webscraper/scraperMaxPage";
 
-export function main() {
-  console.log("Hello, TypeScript!");
+const baseUrl = 'https://webscraper.io/test-sites/e-commerce/static/computers/laptops'
+
+async function main() {
+    const maxPage = await scraperMaxPage(baseUrl)
+
+    for(let page = 1; page <= maxPage; page++) {
+
+        const links = await scraperLinks({
+            webscraperPage: baseUrl,
+            filters: {
+                title: 'lenovo'
+            },
+            page
+        })
+        if (!links) return;
+        const details = (links.map((link) => scraperDetails(link)))
+        
+        console.log(await Promise.all(details));
+    }
 }
 
-main();
+
+main()
+
+
